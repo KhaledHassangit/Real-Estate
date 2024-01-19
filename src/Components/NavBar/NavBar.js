4b602cd0ef7663b7c60be099cd4506146af37868
khaled <khaledhassan199919@gmail.com>
2024-01-19 13:58:55 +0200

    Roll Change

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,11 +9,13 @@ const NavBar = () => {
     return(
         <Navbar expand="lg"  data-bs-theme="dark" className='bg sticky-lg-top ' >
             <Container className='d-flex justify-content-between'>
-                <Roll duration={2000} >
+                
                 <Navbar.Brand href="#home" id='#home'>
+                    <Roll duration={2000} >
                     <img className='logo' src={Logo} alt=''/>
+                    </Roll>
                 </Navbar.Brand>
-                </Roll>
+            
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav  className='ms-auto'>
